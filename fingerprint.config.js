64f512cae160bd05a203c8bd4dd45f5ab3bd1708
thehamsti <john@imp.gg>
2026-01-04 21:50:15 -0700

    Update things

--- fingerprint.config.js
+++ fingerprint.config.js
@@ -1,8 +1,13 @@
 /** @type {import('@expo/fingerprint').Config} */
 module.exports = {
+  sourceSkips: [
+    // Skip bare native directories - they differ between local and EAS due to pod install
+    "ExpoConfigRuntimeVersionRuntimeVersionPolicyBareNativeDir",
+  ],
   ignorePaths: [
     "ios/build/**",
     "ios/Pods/**",
     "ios/.xcode.env.local",
+    "ios/Podfile.lock",
   ],
 };
